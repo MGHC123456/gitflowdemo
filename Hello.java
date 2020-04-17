c0afdc0af8339216b75ea57aa0f0550941d1c256
dev01 <dev01@qq.com>
2020-04-17 20:59:32 +0800

    function01 dev01

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 class Hello {
 	public static void main(String[] args) {
 		System.out.println("inited by SCM");
-		System.out.println("develop branch created by SCM.");
+        	System.out.println("develop branch created by SCM.");
+		System.out.println("develop branch created by dev01.");
 	}
 }
